8f481ac667cf35e059926f1ffe20aa00f23b04fd
implaymo <136201112+implaymo@users.noreply.github.com>
2024-06-10 12:00:19 +0100

    Add while loop to keep the volume at 0.4 if app is running.

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from pycaw.pycaw import AudioUtilities
 
 def change_whatsapp_volume():
     try:
-        sessions = AudioUtilities.GetAllSessions()
-        for session in sessions:
-            volume = session.SimpleAudioVolume
-            if session.Process and session.Process.name() == "Whatsapp.exe":
-                volume.SetMasterVolume(0.5, None)
-                print("Volume adjusted for WhatsApp.")
+        while True:
+            sessions = AudioUtilities.GetAllSessions()
+            for session in sessions:
+                volume = session.SimpleAudioVolume
+                if session.Process and session.Process.name() == "Whatsapp.exe":
+                    volume.SetMasterVolume(0.4, None)
+                    print("Volume adjusted for Whatsapp.")
     except Exception as e:
         print(f"ERROR FOUND: {e}")
 
